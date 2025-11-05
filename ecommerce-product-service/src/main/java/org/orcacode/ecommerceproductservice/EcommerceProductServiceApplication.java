042cf00aae5df2900c0d4b7945dcd746734f7c7e
AlirezaAbolhasani <alireza.ab.1111.com>
2025-11-05 09:26:14 +0330

    Add 4 modules

---
+++ ecommerce-product-service/src/main/java/org/orcacode/ecommerceproductservice/EcommerceProductServiceApplication.java
@@ -0,0 +1,13 @@
+package org.orcacode.ecommerceproductservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceProductServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceProductServiceApplication.class, args);
+    }
+
+}
