167143ddc8d152217bc383c06ac35b9def83ad36
AlirezaAbolhasani <alireza.ab.1111.com>
2025-11-23 11:52:16 +0330

    init master

--- ecommerce-order-service/src/main/java/org/orcacode/ecommerceorderservice/controller/OrderController.java
+++ ecommerce-order-service/src/main/java/org/orcacode/ecommerceorderservice/controller/OrderController.java
@@ -2,13 +2,11 @@ package org.orcacode.ecommerceorderservice.controller;
 
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import org.orcacode.ecommerceorderservice.dto.OrderDto;
+
 import org.orcacode.ecommerceorderservice.model.OrderModel;
 import org.orcacode.ecommerceorderservice.service.OrderService;
 import org.orcacode.ecommerceorderservice.service.OrderServiceManager;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,13 +16,14 @@ import org.springframework.web.bind.annotation.*;
  * ecommerce-microservices-springboot
  * 11/11/2025
  */
-@CrossOrigin(origins = "http://localhost:8081")
+@CrossOrigin(origins = "http://localhost:8084")
 @RestController
-@RequestMapping(path = {"/api/v1"}, produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping("/api")
 public class OrderController {
 
     private final OrderServiceManager orderServiceManager;
     private final OrderService orderService;
+
 
     public OrderController(OrderServiceManager orderServiceManager,OrderService orderService) {
         this.orderServiceManager = orderServiceManager;
@@ -34,33 +33,22 @@ public class OrderController {
     @GetMapping("/orders/{id}")
     @Operation(summary = "Find a user by userID. This number is unique and personally.",
             description = "Id must exist")
-    public ResponseEntity<OrderModel> showOrderInformation(
-            @Parameter(description = "Order ID")
-            @PathVariable(required = true) Long id) {
+    public ResponseEntity<OrderModel> showOrderInformation(@PathVariable(required = true) Long id) {
         return new ResponseEntity<>(orderServiceManager.getOrderInformationByOrderId(id), HttpStatus.OK);
     }
 
     @PostMapping("/orders")
     @Operation(summary = "Find a user by userID. This number is unique and personally.",
             description = "Id must exist")
-    public ResponseEntity<OrderModel> addOrder(
-        @Parameter(description = "Contain order information and also orderItems")
-        @RequestBody(required = true) OrderModel orderModel) {
+    public ResponseEntity<OrderModel> addOrder(@RequestBody(required = true) OrderModel orderModel) {
         return new ResponseEntity<>(orderServiceManager.saveOrder(orderModel), HttpStatus.CREATED);
     }
 
-    @GetMapping("/orders/{id}")
-    @Operation(summary = "Find a user by userID. This number is unique and personally.",
-            description = "Id must exist")
-    public ResponseEntity<OrderDto> getOrder(
-        @Parameter(description = "Order ID")
-        @PathVariable(required = true) Long id) {
-       return new ResponseEntity<>(orderService.getOrderById(id), HttpStatus.OK);
-    }
-
-    @GetMapping("/orders/{id}")
-    public ResponseEntity<String> getInfo(@PathVariable Long id){
-        return new ResponseEntity<>("Hello World",HttpStatus.OK);
-    }
+//    @GetMapping("/orders/{id}")
+//    @Operation(summary = "Find a user by userID. This number is unique and personally.",
+//            description = "Id must exist")
+//    public ResponseEntity<OrderDto> getOrder(@PathVariable(required = true) Long id) {
+//       return new ResponseEntity<>(orderService.getOrderById(id), HttpStatus.OK);
+//    }
 
 }
--- ecommerce-order-service/src/main/java/org/orcacode/ecommerceorderservice/mapper/OrderItemsMapper.java
+++ ecommerce-order-service/src/main/java/org/orcacode/ecommerceorderservice/mapper/OrderItemsMapper.java
@@ -1,8 +1,6 @@
 package org.orcacode.ecommerceorderservice.mapper;
 
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
-import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.*;
 import org.mapstruct.factory.Mappers;
 import org.orcacode.ecommerceorderservice.dto.OrderItemsDto;
 import org.orcacode.ecommerceorderservice.entity.OrderItems;
@@ -11,12 +9,15 @@ import org.orcacode.ecommerceorderservice.entity.OrderItems;
  * Alireza Abolhasani
  * ecommerce-microservices-springboot
  * 11/12/2025
+ * nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS
  */
-@Mapper(componentModel = "spring"
-        ,nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-public interface OrderItemsMapper {
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING
+       ,nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+public
+
+interface OrderItemsMapper {
     OrderItemsMapper INSTANCE = Mappers.getMapper(OrderItemsMapper.class);
     OrderItemsDto toDto(OrderItems orderItems);
     OrderItems toEntity(OrderItemsDto orderItems);
-    OrderItemsDto updateFromDto(OrderItemsDto dto , @MappingTarget OrderItems entity);
+//    OrderItemsDto updateFromDto(OrderItemsDto dto , @MappingTarget OrderItems entity);
 }
