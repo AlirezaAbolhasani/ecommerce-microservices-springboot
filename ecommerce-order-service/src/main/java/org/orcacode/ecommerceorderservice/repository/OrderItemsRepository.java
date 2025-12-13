f06eeb41f78719bfef88938b0e6f2d05c4f09bb7
AlirezaAbolhasani <alireza.ab.1111.com>
2025-12-13 14:42:17 +0330

    1

--- ecommerce-order-service/src/main/java/org/orcacode/ecommerceorderservice/repository/OrderItemsRepository.java
+++ ecommerce-order-service/src/main/java/org/orcacode/ecommerceorderservice/repository/OrderItemsRepository.java
@@ -3,7 +3,6 @@ package org.orcacode.ecommerceorderservice.repository;
 import org.orcacode.ecommerceorderservice.entity.OrderItems;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
